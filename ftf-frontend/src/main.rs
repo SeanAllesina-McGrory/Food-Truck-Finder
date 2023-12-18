cda774fce515b2aa86b728d99f0ac07fac153f50
SeanAllesina-McGrory <charlie@SKYRIM10.>
2023-12-17 23:20:52 -0800

    Basic Frontend
    Added basic front end integration with facebook oauth.
    Very preliminary, not settled on the security structure due to not being 100 on the threat model.
    Will update in the future. Need to move secrets from env vars to a dotenv.

--- ftf-frontend/src/main.rs
+++ ftf-frontend/src/main.rs
@@ -1,3 +1,61 @@
+use gloo::console::log;
+use reqwasm::http::Request;
+use serde_derive::{Deserialize, Serialize};
+use serde_json::{json, to_string_pretty};
+use std::env;
+use yew::prelude::*;
+
+#[derive(Serialize)]
+pub struct Auth {
+    pub token: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Vendor {
+    pub uuid: Option<String>,
+    pub name: Option<String>,
+    pub auth_token: Option<String>,
+    pub description: Option<String>,
+    pub vendor_type: Option<String>,
+    pub email: Option<String>,
+    pub phone_number: Option<String>,
+    pub website: Option<String>,
+}
+
+#[function_component(App)]
+fn app() -> Html {
+    let state = use_state(|| Auth {
+        token: String::from("1234"),
+    });
+
+    let onclick = {
+        let state = state.clone();
+        Callback::from(move |_| {
+            let state = state.clone();
+            wasm_bindgen_futures::spawn_local(async move {
+                let response = Request::get("http://localhost:8081/v1/vendors")
+                    .header("", &state.token)
+                    .send()
+                    .await
+                    .unwrap()
+                    .json::<Vec<Vendor>>()
+                    .await
+                    .unwrap();
+
+                log!(to_string_pretty(&response).unwrap());
+            });
+        })
+    };
+
+    html! {
+        <>
+            <h1>{ "Hello, Cruel World!" }</h1>
+            <button {onclick}>{"get vendors"}</button>
+            <a class="button" href={format!("https://www.facebook.com/v18.0/dialog/oauth?client_id=345272141598641&&redirect_uri=http://localhost:8080/auth&state={}", json!(*state))} aria-label="Learn about the HTML button tag"><span>{"HTML button tag"}</span></a>
+        </>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
 }
