27bf76827f6bcdca347606d04f3458662f1ad809
SeanAllesina-McGrory <charlie@SKYRIM10.>
2023-12-24 20:14:37 -0800

    Crate files

---
+++ server/src/server.rs
@@ -0,0 +1,8 @@
+#[path = "server/app.rs"]
+pub mod app;
+#[path = "server/handlers.rs"]
+pub mod handlers;
+#[path = "server/middleware.rs"]
+pub mod middleware;
+#[path = "server/state.rs"]
+pub mod state;
---
+++ server/src/utils.rs
@@ -0,0 +1,6 @@
+#[path = "utils/auth.rs"]
+pub mod auth;
+#[path = "utils/token.rs"]
+pub mod token;
+#[path = "utils/verify.rs"]
+pub mod verify;
